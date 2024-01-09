30c6a88c2663615c93df10bdfc4a81736fcf8994
Thang <loquangthang07@gmail.com>
2024-01-09 18:12:40 +0700

    Frontend Project Template

---
+++ RealEstateAuctionSystem/reas-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="inline-block px-5 py-2 text-3xl font-bold underline border border-blue-300 rounded-lg">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
