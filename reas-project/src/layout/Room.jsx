9b49ae33c2c3a643967fd350f5db854fc44e0ed7
Thang <loquangthang07@gmail.com>
2024-03-15 00:12:22 +0700

    Fix auction room 6th

--- reas-project/src/layout/Room.jsx
+++ reas-project/src/layout/Room.jsx
@@ -83,7 +83,6 @@ const Room = () => {
     });
 
     getConfirmBid();
-    const roomStatus = getStatus(); // Get the room status
     // setIsOngoing(true);
 
     return unsubscribe; // Use this for cleanup later
@@ -108,88 +107,104 @@ const Room = () => {
     if (docSnap.exists()) {
       const status = docSnap.data().status;
       console.log("Status:", status);
-      // setIsOngoing(status);
-      return status === "ongoing";
+      setIsOngoing(status);
+      return status;
     } else {
       console.log("Document not found!");
       // Handle case where the document might not exist
     }
   };
+
+  const handleClose = () => {
+    setIsOngoing(false);
+    console.log(isOngoing);
+  };
   return (
     <>
-      <div>
-        <h1 className="mb-10 text-2xl font-bold">{`Welcome to ${roomName}, Mr.${user.userInfo.username}`}</h1>
-        <div className="flex items-center justify-between px-10">
+      {!isOngoing ? (
+        <div>
           <Link to="/">
-            <button
-              className="px-2 py-2 bg-red-500 rounded"
-              onClick={handleOutRoom}
-            >
-              <span className="flex items-center justify-between gap-1 text-white">
-                <IoLogOut color="white" size="20" />
-                Thoát
-              </span>
-            </button>
+            <button className="px-5 py-2 bg-blue-400 rounded-lg">Out</button>
           </Link>
         </div>
-        <div className="flex p-10">
-          <div className="basis-[70%]">
-            <div className="flex">
-              <div className="basis-[50%]">
-                <div className="pr-5">
-                  <img
-                    src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"
-                    alt=""
-                    className="w-full h-[500px] rounded-xl"
-                  />
-                </div>
-              </div>
-              <div className="basis-[50%] pr-5">
-                <div className="bg-slate-900 rounded-xl h-[500px] p-3">
-                  <UserGrid roomName={roomName} userName={userName} />
-                </div>
-                <h1>
-                  {bidAmount.amount} lần thứ {bidTimes}
-                </h1>
-                {winner && (
-                  <div className="fixed z-50 p-4 text-white -translate-x-1/2 bg-green-500 rounded-lg top-16 left-1/2">
-                    <h1>Winner!</h1>
-                    <p>
-                      Congratulations,{" "}
-                      <span className="font-bold">{bidAmount.userName}</span>{" "}
-                      won with a bid of {bidAmount.amount}
-                    </p>
+      ) : (
+        <div>
+          <h1 className="mb-10 text-2xl font-bold">{`Welcome to ${roomName}, Mr.${user.userInfo.username}`}</h1>
+          <div className="flex items-center justify-between px-10">
+            <Link to="/">
+              <button
+                className="px-2 py-2 bg-red-500 rounded"
+                onClick={handleOutRoom}
+              >
+                <span className="flex items-center justify-between gap-1 text-white">
+                  <IoLogOut color="white" size="20" />
+                  Thoát
+                </span>
+              </button>
+            </Link>
+          </div>
+          <div className="flex p-10">
+            <div className="basis-[70%]">
+              <div className="flex">
+                <div className="basis-[50%]">
+                  <div className="pr-5">
+                    <img
+                      src="https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"
+                      alt=""
+                      className="w-full h-[500px] rounded-xl"
+                    />
                   </div>
-                )}
-                {user.userInfo.role === "Admin" ? (
-                  <ConfirmBox
-                    roomName={roomName}
-                    currentBid={bidAmount}
-                    userName={bidAmount.userName}
-                  />
-                ) : (
-                  <BidBox
-                    userName={bidAmount.userName}
-                    currentBid={bidAmount}
-                    roomName={roomName}
-                    bidTimes={bidTimes}
-                  />
-                )}
+                </div>
+                <div className="basis-[50%] pr-5">
+                  <div className="bg-slate-900 rounded-xl h-[500px] p-3">
+                    <UserGrid roomName={roomName} userName={userName} />
+                  </div>
+                  <h1>
+                    {bidAmount.amount} lần thứ {bidTimes}
+                  </h1>
+                  {winner && (
+                    <div className="fixed z-50 p-4 text-white -translate-x-1/2 bg-green-500 rounded-lg top-16 left-1/2">
+                      <h1>Winner!</h1>
+                      <p>
+                        Congratulations,{" "}
+                        <span className="font-bold">{bidAmount.userName}</span>{" "}
+                        won with a bid of {bidAmount.amount}
+                      </p>
+                    </div>
+                  )}
+                  {user.userInfo.role === "Admin" ? (
+                    <>
+                      <ConfirmBox
+                        roomName={roomName}
+                        currentBid={bidAmount}
+                        userName={bidAmount.userName}
+                      />
+                      <button onClick={handleClose}>Close</button>
+                    </>
+                  ) : (
+                    <BidBox
+                      userName={bidAmount.userName}
+                      currentBid={bidAmount}
+                      roomName={roomName}
+                      bidTimes={bidTimes}
+                    />
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="basis-[30%]  bg-white h-[750px]">
-            <div className="overflow-hidden border rounded">
-              <ChatTabs
-                roomName={roomName}
-                userName={userName}
-                role={role}
-                userId={userId}
-              />
+            <div className="basis-[30%]  bg-white h-[750px]">
+              <div className="overflow-hidden border rounded">
+                <ChatTabs
+                  roomName={roomName}
+                  userName={userName}
+                  role={role}
+                  userId={userId}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
